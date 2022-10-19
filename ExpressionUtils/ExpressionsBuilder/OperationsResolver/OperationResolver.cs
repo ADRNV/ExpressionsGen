06ea93c9c6fc6a7de3c1731c146a10d4a66b636d
NKT <49834917+ADRNV@users.noreply.github.com>
2022-10-19 18:49:06 +0300

    Corrected one stupid error

--- ExpressionUtils/ExpressionsBuilder/OperationsResolver/OperationResolver.cs
+++ ExpressionUtils/ExpressionsBuilder/OperationsResolver/OperationResolver.cs
@@ -17,23 +17,7 @@ namespace ExpressionUtils.ExpressionsBuilder.OperationsResolver
 
         public override Expression Resolve(Expression left, ExpressionType? operation, Expression right)
         {
-            switch (operation)
-            {
-                case ExpressionType.Add:                    
-                     return Expression
-                       .Add(left, right);
-                case ExpressionType.Subtract:
-                    return Expression
-                     .Subtract(left, right);
-                case ExpressionType.Multiply:
-                    return Expression
-                     .Multiply(left, right);
-                case ExpressionType.Divide:
-                    return Expression
-                     .Subtract(left, right);
-                default:
-                    throw new InvalidOperationException();
-            }
+            return Expression.MakeBinary((ExpressionType)operation, left, right);
         }
     }
 }
